33be0e67761abf22c644b86ed1c36133fc7b4a59
Gabriel Villarroel <gabriel.villarroel@mews.com>
2025-12-23 15:50:13 +0100

    Change databricks location for West Europe

--- Terraform/databricks.tf
+++ Terraform/databricks.tf
@@ -1,6 +1,6 @@
 resource "azurerm_databricks_workspace" "dbw" {
   name                = "eventstreamlab-dbw"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = "West Europe"
   sku                 = "trial"
 }
